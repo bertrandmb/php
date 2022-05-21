f8c204ac92b1f2a5998576f07c281dded463ee39
bertrandmb <101927042+bertrandmb@users.noreply.github.com>
2022-05-21 22:03:48 +0200

    Create zahl_variable.php

---
+++ zahl_variable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $ ganzezahl = -14;
+    $ kommazahl = 1,15;
+    $ grosseZahl = 5.5e6;
+    $ separatorGross = 1_580_000_000;
+
+    echo ganzezahl . "<br>";
+    echo kommazahl . "<br>";
+    echo grosseZahl . "<br";
+    echo separatorGross . "<br";
+
+    ?>
+</body>
+</html>
